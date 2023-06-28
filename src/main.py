ae994980f0668e540df69c0b01270117d276795a
malloc-nbytes <zdhdev@yahoo.com>
2023-06-28 15:27:55 -0500

    More better logging

--- src/main.py
+++ src/main.py
@@ -21,10 +21,10 @@ TEMPLATE_FILE = None
 def validate():
     global COMPILER, DIRECTORY, FILENAME, TEMPLATE_FILE
     error_msgs = [
-        '(!) Compiler is not specified',
-        '(!) Filepath is not specified',
-        '(!) Template is not specified',
-        '(!) Invalid file extension. File extension should be .tex'
+        '(ERR) Compiler is not specified',
+        '(ERR) Filepath is not specified',
+        '(ERR) Template is not specified',
+        '(ERR) Invalid file extension. File extension should be .tex'
     ]
 
     isvalid = True
@@ -52,69 +52,71 @@ def validate():
 
 def usage(msg=None):
     if msg is not None:
-        print("(!)", msg)
-    print("(!) Usage: python3 main.py template=<template> filepath=~/path/to/destination/main.tex compiler=<compiler>")
-    print("(!) Use the --help flag for descriptions")
+        print("(ERR)", msg)
+    print("(USAGE) python3 main.py template=<template> filepath=~/path/to/destination/main.tex compiler=<compiler>")
+    print("(HELP) Use the --help flag for descriptions")
     exit(1)
 
 def help():
-    print("(+) Flags can be specified in any order")
-    print("(+) template=<template>")
-    print("(+)   `article`")
-    print("(+)     - Creates a template in `article` format")
-    print("(+)   `research`")
-    print("(+)     - Creates a template in `research` format")
-    print("(+)   `homework`")
-    print("(+)     - Creates a template in `homework` format")
-    print("(+)   `notes`")
-    print("(+)     - Creates a template in `notes` format")
-    print("(+) filepath=<path/to/destination/name.tex>")
-    print("(+)   - Provide a path to create the files")
-    print("(+)   - If the directory does not exist, it will create it")
-    print("(+)   - The filename must end in `.tex`")
-    print("(+) compiler=<compiler>")
-    print("(+)   - Specify the compiler that the Makefile will use")
-    print("(+)   - An example one is `pdflatex`")
+    print("(HELP) Flags can be specified in any order")
+    print("(HELP) template=<template>")
+    print("(HELP)   `article`")
+    print("(HELP)     - Creates a template in `article` format")
+    print("(HELP)   `research`")
+    print("(HELP)     - Creates a template in `research` format")
+    print("(HELP)   `homework`")
+    print("(HELP)     - Creates a template in `homework` format")
+    print("(HELP)   `notes`")
+    print("(HELP)     - Creates a template in `notes` format")
+    print("(HELP) filepath=<path/to/destination/name.tex>")
+    print("(HELP)   - Provide a path to create the files")
+    print("(HELP)   - If the directory does not exist, it will create it")
+    print("(HELP)   - The filename must end in `.tex`")
+    print("(HELP) compiler=<compiler>")
+    print("(HELP)   - Specify the compiler that the Makefile will use")
+    print("(HELP)   - An example one is `pdflatex`")
 
 def set_compiler(value):
     global COMPILER
     COMPILER = value
-    print(f"(+) Set compiler to {value}")
+    print(f"Set compiler to {value}")
 
 def set_filepath(location):
     global DIRECTORY, FILENAME
     DIRECTORY, FILENAME = os.path.split(location)
-    print(f"(+) Set filepath to {location}")
+    print(f"Set directory to {DIRECTORY}")
+    print(f"Set filename to {FILENAME}")
+    print(f"Set filepath to {location}")
 
 def set_template(value):
     global FILES, TEMPLATE_FILE
     try:
         TEMPLATE_FILE = open(FILES[value], "r")
     except:
-        print(f"(!) Invalid template {value}")
+        print(f"(ERR) Invalid template {value}")
         return
-    print(f"(+) Set template to {value}")
+    print(f"Set template to {value}")
 
-def create_dir(directory, filepath, content, name):
+def create_dir(directory, filepath, content):
     try:
         os.makedirs(directory, exist_ok=True)
         with open(filepath, "w") as f:
             f.write(content)
-            print(f"(+) {name} created successfully")
+            print(f"{filepath} created successfully")
     except OSError as e:
-        print("(!) An error occurred while creating the directory:", str(e))
+        print("(ERR) An error occurred while creating the directory:", str(e))
     except Exception as e:
-        print("(!) An error occurred while creating the Makefile:", str(e))
+        print("(ERR) An error occurred while creating the Makefile:", str(e))
 
 def create_makefile(directory, filename, compiler):
     filepath = os.path.join(directory, "Makefile")
     content = filename.split('.')[0] + ': ' + filename + '\n\t' + compiler + ' ' + filename
-    create_dir(directory, filepath, content, "Makefile")
+    create_dir(directory, filepath, content)
 
 def create_template(directory, filename, template_file):
     filepath = os.path.join(directory, filename)
     content = template_file.read()
-    create_dir(directory, filepath, content, filename)
+    create_dir(directory, filepath, content)
 
 set_flag = {
     "compiler": set_compiler,
@@ -125,11 +127,15 @@ set_flag = {
 argv = sys.argv[1:]
 argc = len(sys.argv) - 1
 
+if argc == 0:
+    usage()
+
+if argv[0] == "--help":
+    help()
+    exit(1)
+
 for i in range(0, argc):
     try:
-        if argv[i] == "--help":
-            help()
-            break
         flag, value = argv[i].split('=')
         set_flag[flag](value)
     except:
@@ -139,3 +145,4 @@ validate()
 
 create_makefile(DIRECTORY, FILENAME, COMPILER)
 create_template(DIRECTORY, FILENAME, TEMPLATE_FILE)
+print("Finished template creation")
